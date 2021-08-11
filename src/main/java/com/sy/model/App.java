60ca1b163cadef71e693d567bf483c318cd1d5f3
czx <18932200163@163.com>
2021-08-11 16:54:18 +0800

    修用户注销bug，面试宝典二位码显示bug

--- src/main/java/com/sy/model/App.java
+++ src/main/java/com/sy/model/App.java
@@ -1,10 +1,11 @@
 package com.sy.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
-
+@Data
 public class App {
     private Integer id;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
@@ -15,46 +16,13 @@ public class App {
 
     private String title;
 
-    public App(Integer id, Date creatime, String describe, String title) {
+    private Integer downloadid;
+
+    public App(Integer id, Date creatime, String describe, String title, Integer downloadid) {
         this.id = id;
         this.creatime = creatime;
         this.describe = describe;
         this.title = title;
-    }
-
-    public App() {
-        super();
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Date getCreatime() {
-        return creatime;
-    }
-
-    public void setCreatime(Date creatime) {
-        this.creatime = creatime;
-    }
-
-    public String getDescribe() {
-        return describe;
-    }
-
-    public void setDescribe(String describe) {
-        this.describe = describe == null ? null : describe.trim();
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title == null ? null : title.trim();
+        this.downloadid = downloadid;
     }
 }
