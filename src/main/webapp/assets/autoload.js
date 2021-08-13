0b5e0ef3d1386df2acc1230fc4d80e1f89ab2463
czx <18932200163@163.com>
2021-08-13 09:57:18 +0800

    离职更新

--- src/main/webapp/assets/autoload.js
+++ src/main/webapp/assets/autoload.js
@@ -5,8 +5,8 @@ $.ajax({url: 'assets/live2d.min.js?v=1.0.5',dataType:"script", cache: true, asyn
 
 /* 可直接修改部分参数 */
 live2d_settings['hitokotoAPI'] = 'jinrishici.com'; // 一言 API
-live2d_settings['modelId'] = 2;                    // 默认模型 ID
-live2d_settings['modelTexturesId'] = 5;            // 默认材质 ID
+live2d_settings['modelId'] = 6;                    // 默认模型 ID
+live2d_settings['modelTexturesId'] = 1;            // 默认材质 ID
 live2d_settings['modelStorage'] = false;           // 不储存模型 ID
 live2d_settings['canCloseLive2d'] = true;         // 隐藏 关闭看板娘 按钮
 live2d_settings['canTurnToHomePage'] = true;      // 隐藏 返回首页 按钮
@@ -16,7 +16,7 @@ live2d_settings['waifuFontSize'] = '20px';         // 提示框字体
 live2d_settings['waifuEdgeSide'] = 'right:100';     // 看板娘贴边方向
 live2d_settings['waifuToolFont'] = '18px';         // 工具栏字体
 live2d_settings['waifuToolLine'] = '30px';         // 工具栏行高
-live2d_settings['waifuToolTop'] = '0px';         // 工具栏顶部边距
+live2d_settings['waifuToolTop'] = '-20px';         // 工具栏顶部边距
 live2d_settings['waifuDraggable'] = 'axis-x';      // 拖拽样式
 
 /* 内置 waifu-tips.json */
