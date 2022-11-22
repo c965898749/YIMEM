d7c20cb1a64a80b08141b5cac8427f8d2edf509b
czx <965898749@qq.com>
2022-11-22 17:10:47 +0800

    自适应缩放

--- src/main/webapp/js/setCookie.js
+++ src/main/webapp/js/setCookie.js
@@ -1,3 +1,56 @@
+// * 默认缩放值
+const scale = {
+    width: '1',
+    height: '1',
+};
+
+// * 设计稿尺寸（px）
+const baseWidth = 1920;
+const baseHeight = 1080;
+
+// const baseWidth = 1380;
+// const baseHeight = 782;
+
+// const baseWidth = 1565;
+// const baseHeight = 865;
+
+
+// * 需保持的比例（默认16:9）
+const baseProportion = parseFloat((baseWidth / baseHeight).toFixed(5));
+
+window.addEventListener('resize', this.resize);
+
+function resize() {
+    const appRef = this.$refs['appRef'];
+    if (!appRef) return;
+    // 当前宽高比
+    const currentRate = parseFloat(
+        (window.innerWidth / window.innerHeight).toFixed(5)
+    );
+    if (appRef) {
+        if (currentRate > baseProportion) {
+            // 表示更宽
+            scale.width = (
+                (window.innerHeight * baseProportion) /
+                baseWidth
+            ).toFixed(5);
+            scale.height = (window.innerHeight / baseHeight).toFixed(5);
+            // appRef.style.transform = `scale(${scale.width}, ${scale.height}) translate(-50%, -50%)`;
+            appRef.style.transform = `scale(${scale.width}, ${scale.height})`;
+        } else {
+            // 表示更高
+            scale.height = (
+                window.innerWidth /
+                baseProportion /
+                baseHeight
+            ).toFixed(5);
+            scale.width = (window.innerWidth / baseWidth).toFixed(5);
+            // appRef.style.transform = `scale(${scale.width}, ${scale.height}) translate(-50%, -50%)`;
+            appRef.style.transform = `scale(${scale.width}, ${scale.height})`;
+        }
+    }
+}
+
 function getCookie(c_name)
 {
     if (document.cookie.length>0)
